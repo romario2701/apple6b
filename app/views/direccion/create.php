4097929ea3c4f426b64b339ed898be8228f8cf69
Stalin francis <educaysoft@gmail.com>
2025-05-12 14:39:40 -0500

    mejmras

--- app/views/direccion/create.php
+++ app/views/direccion/create.php
@@ -1,5 +1,15 @@
-<form action="../../controllers/SexoController.php?action=create" method="POST">
-    <label for="nombre">Nombre:</label>
+<form action="../../controllers/DireccionController.php?action=create" method="POST">
+    <label for="idpersona">Persona:</label>
+    <select name="idpersona" id="idpersona" required>
+        <?php foreach ($personas as $persona): ?>
+            <option value="<?= $persona['idpersona'] ?>">
+                <?= htmlspecialchars($persona['nombres'] . ' ' . $persona['apellidos']) ?>
+            </option>
+        <?php endforeach; ?>
+    </select>
+
+    <label for="nombre">Dirección:</label>
     <input type="text" name="nombre" id="nombre" required>
+
     <input type="submit" value="Crear">
 </form>
